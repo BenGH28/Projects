a8349ecaa887b79af5de4572d25b0e100448e816
Ben <bigben.hunt@gmail.com>
2019-04-11 21:35:39 -0600

    I finished it!

--- encryption/encrypt1.cc
+++ encryption/encrypt1.cc
@@ -17,49 +17,98 @@
 
 #include<iostream>
 #include<string>
+#include<queue>
 #include<fstream>
 using namespace std;
 
+void toAscii(string&);
+void onQ(string&, queue<int>&);
+void readQ(queue<int>);
+void encrypt(string&);
+void decrypt(string&, queue<int>&);
 
 int main()
 {
-  fstream file;
-  // ofstream ofile;
-  string fname, s;
+  queue<int> decr;
+  string message, enMess;
   char ans;
-
-  cout <<"What file would you like to encrypt?"<<endl;
-  cin >> fname;
-  file.open(fname);
-
-  if(file.fail())
-    cerr << "error opening" << endl;
-  else
-    {
-      while(getline(file, s))
-	{
-	  for(int i = 0; i < s.size(); i++)
-	    {
-	      cout<< static_cast<int>(s[i]);
-	      file << static_cast<int>(s[i]);
-	      file<< "hello"; 
-	    }
-	}
-    }
-  /* cout << "would you like to change it back? (Y/N) ";
-  cin >> ans;
-  if(ans == tolower('Y'))
-    {
-      for(int i = 0; i < s.length(); i++)
-	file<< static_cast<char>(s[i]);
-	}*/
-  file.close();
+  int choice;
+  cout << "Press 1 for encryption or 2 for decryption: ";
+  cin >> choice;
   
+  if(choice == 1){
+    encrypt(message);
+  }
+  else{
+    decrypt(enMess, decr);
+  }
   return 0;
+    
 }
 
 		 
+void toAscii(string& a)
+{
+  for(int i = 0; i < a.size(); i++){
+    cout<< static_cast<int>(a[i]) << " ";
+  }
+      cout << endl;
+}
+  
+void onQ(string& a, queue<int>& b){
+  int j = 0;
+  string sub;
+  int num;
+  for(int i = 0; i < a.size(); i++){
+    if(a[i] == ' ' && j == 0){ //beginning edge case	
+      sub = a.substr(j,i);
+      num = stoi(sub);
+      b.push(num);
+      j = i+1;
+    }
+    else if(i == a.size()-1){ //end edge case
+      sub = a.substr(j, ((i-j)+1));
+      num = stoi(sub);
+      b.push(num);
+    }
+    else if(a[i] == ' '){
+      sub = a.substr(j,i-j );
+      num = stoi(sub);
+      b.push(num);
+      j = i + 1;
+    }
+  }
+}
+void readQ(queue<int> q){
+  int asciiInt;
+  char asciiChar;
+  string dMess = "";
+  while(!q.empty()){
+    asciiInt = q.front();
+    asciiChar = asciiInt;
+    dMess += asciiChar;
+    q.pop();
+  }
+  cout << "Your message is: " << dMess << endl;
+}
+void encrypt(string& a){
+  cout <<"Enter the message you would like to encrypt?"<<endl;
+  cin.ignore();
+  getline(cin, a);
+  toAscii(a);
+}
+void decrypt(string& a, queue<int>& q){
+ cout <<"Enter the encrypted message below." << endl;
+ cin.ignore();
+ getline(cin, a);
+ onQ(a, q);
+ readQ(q);
+}
 
+
+    
+  
+  
   
   
   
