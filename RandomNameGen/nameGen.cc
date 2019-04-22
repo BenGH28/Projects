83503d2ec9b7b0600207c16c04ef45c6602f0915
Ben <bigben.hunt@gmail.com>
2019-04-22 13:58:18 -0600

    Now carefully and efficiently optimized for your random name generation pleasure

--- RandomNameGen/nameGen.cc
+++ RandomNameGen/nameGen.cc
@@ -7,66 +7,37 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+#include<cctype>
 using namespace std;
-
-void print(char ch[], int a){
-  for(int i = 0; i < a; i++)
-    cout << ch[i];
-  cout << endl;
-}
 
 int main()
 {
   srand(time(0));
-  int size = rand()%8 + 3; //random size of name array from 3 to 10
-  int first = 0, next = 1;
-  char vow[5] = {'a','e','i','o','u'};
+  unsigned int size = rand() % 8 + 3; //random size of name array from 3 to 10
+  char vow[6] = {'a','e','i','o','u','y'};
   char con[21] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
-  char name[size];
-  int choice = rand()%100;//random number between 0 and 100 to choose
-                          //what letter the name begins with
+  char name[size+1];
+  int choice = rand()%2; //choose if the name starts with vowel or consonant
   cout << "Choice: " << choice << endl
        << "Size: " << size << endl;
-/*  while (first < size) {
-    if(choice % 2 == 0){
-      name[first] = vow[rand()%6];
-      name[next] = con[rand()%22];
-      first += 2;
-      next += 2;
-      if (next == size)
-        name[first] = vow[rand()%6];
-    }
-    else{
-      name[first] = con[rand()%22];
-      name[next] = vow[rand()%6];
-      first += 2;
-      next += 2;
-      if (next == size)
-        name[first] = vow[rand()%6];
-    }
-  }*/
-  if(choice % 2 == 0){
-    for(int i = 0; i < size; i++){
-      name[first] = vow[rand()%6];
-      name[next] = con[rand()%22];
-      if (next < size){
-        first += 2;
-        next += 2;
-      }
-      cout << "First: " << first << " Next: " << next << endl;
+  if(choice == 0){
+    for(unsigned int i = 0; i < size; i++){
+      if(i % 2 == 0)
+        name[i] = vow[rand() % 6];
+      else
+        name[i] = con[rand() % 21];
     }
   }
   else{
-    for(int i = 0; i < size; i++){
-      name[first] = con[rand()%22];
-      name[next] = vow[rand()%6];
-      if (next < size){
-        first += 2;
-        next += 2;
-      }
-      cout << "First: " << first << " Next: " << next << endl;
+    for(unsigned int i = 0; i < size; i++){
+      if(i % 2 ==0 )
+        name[i] = con[rand() % 21];
+      else
+        name[i] = vow[rand() % 6];
     }
   }
-  print(name,size);
+  name[size] = '\0';
+  name[0] = toupper(name[0]);
+  cout <<"Your name is: " << name << endl;
   return 0;
 }
