eebd5c9a0e85faa718a5f7e515064a24baa7f39b
Ben <bigben.hunt@gmail.com>
2020-01-11 22:06:11 -0700

    updated

--- RSA/src/RSA.py
+++ RSA/src/RSA.py
@@ -1,4 +1,4 @@
-"""Where RSA cryptography takes place"""
+# """Where RSA cryptography takes place"""
 
 #!/usr/bin/env python3
 import generator as g
@@ -7,47 +7,46 @@ import sys
 
 
 def intKey(key):
-    if key.find(',') != -1:
-        newK = key.split(',')
-        e = int(newK[0])
-        n = int(newK[1])
-        k = (e, n)
-        return k
-    return -1
+    try:
+        if key.find(',') != -1:
+            newK = key.split(',')
+            e = int(newK[0])
+            n = int(newK[1])
+            k = (e, n)
+            return k
+        return -1
+    except:
+        print(f'invalid key')
+        sys.exit()
 
 
-def checkKey(key):
-    if len(key) == 2:
-        if key[0] is int and key[1] is int:
-            return True
-    return False
-
-
-def handle(argv, file):
-    key = intKey(argv[2])
-    if key != -1 and checkKey(key):
+def crypto(file, key):
+    toWrite = ""
+    with open(sys.argv[3], 'r') as f:
+        data = f.readlines()
+    data = [x.strip() for x in data]
+    for line in data:
+        member = line.split(' ')
+        for value in member:
+            if int(value) == 0:
+                continue
+            if sys.argv[1] == '-e':
+                cipher = encrypt(int(value), key)
+                toWrite += str(cipher) + " "
+            elif sys.argv[1] == '-d':
+                decipher = decrypt(int(value), key)
+                toWrite += str(decipher) + " "
+        file.write(toWrite.strip())
+        file.write('\n')
         toWrite = ""
-        with open(argv[3], 'r') as f:
-            data = f.readlines()
-        data = [x.strip() for x in data]
-        for line in data:
-            member = line.split(' ')
-            for value in member:
-                if int(value) == 0:
-                    pass
-                if argv[1] == '--e':
-                    cipher = encrypt(int(value), key)
-                    toWrite += str(cipher) + " "
-                elif argv[1] == '--d':
-                    decipher = decrypt(int(value), key)
-                    toWrite += str(decipher) + " "
-            file.write(toWrite.strip())
-            file.write('\n')
-            toWrite = ""
-        file.close()
-    else:
-        print("Error: invalid key. Valid format is numeric (base 10) CSV notation.\n"
-              "     e.g. 19,45")
+
+
+def workOnFile(fileName, key):
+    try:
+        with open(fileName, 'w') as c:
+            crypto(c, key)
+    except FileNotFoundError:
+        print(f"ERROR: '{sys.argv[3]}' does not exist.")
 
 
 def encrypt(m, PU):
@@ -63,32 +62,45 @@ def decrypt(cipher, PR):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 2:
+    words = ("Commands:\n"
+             "   -g     Generate encryption keys and write\n"
+             "           them to a document of your choosing.\n"
+             "               e.g. RSA.py -g file_To_write_to.txt\n\n"
+
+             "   -e     Encrypt using [key] on [file].\n"
+             "               e.g. RSA.py -e Public,key file_To_Encrypt.txt\n\n"
+
+             "   -d     decrypt a file with a specified key.\n"
+             "               e.g. RSA.py -d Private,key file_To_Decrypt.txt")
+    if len(sys.argv) < 2:
         print(
             "General Usage:\n    RSA.py <command> [key] [file]\n"
         )
-        print(
-            "Commands:\n"
-            "   -g     Generate encryption keys and write\n"
-            "          them to a document of your choosing.\n"
-            "               e.g. RSA.py -g file_To_write_to.txt\n\n"
-
-            "   -e     Encrypt using [key] on [file].\n"
-            "               e.g. RSA.py -e Public,key file_To_Encrypt.txt\n\n"
-
-            "   -d     decrypt a file with a specified key.\n"
-            "               e.g. RSA.py -d Private,key file_To_Decrypt.txt"
-        )
+        print(words)
     elif len(sys.argv) == 3:
         if sys.argv[1] == '-g':
             PU, PR = g.gen(8)
             with open(sys.argv[2], 'w+') as f:
                 f.write(f"Public key: {PU}\nPrivate key: {PR}")
+        else:
+            print(f"Invalid command. Did you mean to use '-g'?\n{words}")
 
     elif len(sys.argv) == 4:
+        key = intKey(sys.argv[2])
         if sys.argv[1] == '-e':
-            ciphertext = open("output.txt", 'w')
-            handle(sys.argv, ciphertext)
-        if sys.argv[1] == '-d':
-            message = open("message.txt", "w")
-            handle(sys.argv, message)
+            if key != -1:
+                workOnFile('output.txt', key)
+            else:
+                print(
+                    f"key:{sys.argv[2]} is not valid numeric format.\n   e.g. 11,17")
+        elif sys.argv[1] == '-d':
+            if key != -1:
+                workOnFile('message.txt', key)
+            else:
+                print(
+                    f"key: {sys.argv[2]} is not valid numeric format.\n   e.g. 11,17")
+        else:
+            print(f"Invalid Command: {sys.argv[1]}\n{words}")
+    else:
+        print(
+            f"ERROR: expected 3 arguments received {len(sys.argv) - 1}\n {words}")
